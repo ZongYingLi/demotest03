3169a1e85cfab1341cfe71b0222be21223d46689
ZongyingLi <2205601179@qq.com>
2021-04-07 09:00:17 +0800

    Initial commit

---
+++ src/main/java/com/example/demotest03/Demotest03Application.java
@@ -0,0 +1,13 @@
+package com.example.demotest03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demotest03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demotest03Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demotest03/Demotest03ApplicationTests.java
@@ -0,0 +1,21 @@
+package com.example.demotest03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+@SpringBootTest
+class Demotest03ApplicationTests {
+
+    @Autowired
+    DataSource dataSource;
+    @Test
+    void contextLoads() throws SQLException {
+        System.out.println(dataSource.getClass());
+        System.out.println(dataSource.getConnection());
+    }
+
+}
